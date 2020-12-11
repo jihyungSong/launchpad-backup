797edec56fe009f59b5aee7b57ba666a04b0a820
Jihyung Song <bluese@megazone.com>
2020-12-11 11:44:57 +0900

    merge all of logic to main.tf

--- spaceone/mongodb/terraform/main.tf
+++ spaceone/mongodb/terraform/main.tf
@@ -7,3 +7,42 @@ provider "template" {}
 provider "aws"{
     region              = var.region
 }
+
+// Security Group for MongoDB
+module "mongodb_security_group" {
+  source      = "./modules/security_group"
+
+  environment = var.environment
+  vpc_id      = var.vpc_id
+
+  mongodb_bastion_ingress_rule_admin_access_security_group_id = var.mongodb_bastion_ingress_rule_admin_access_security_group_id
+  mongodb_bastion_ingress_rule_admin_access_port              = var.mongodb_bastion_ingress_rule_admin_access_port
+  mongodb_app_ingress_rule_mongodb_access_security_group_id   = var.mongodb_app_ingress_rule_mongodb_access_security_group_id
+}
+
+// MongoDB Shard Cluster
+module "shard_cluster" {
+  source                              =   "./modules/shard_cluster"
+
+  environment                         =   var.environment
+
+  // MongoDB Bastion
+  mongodb_ami_id                      =   var.mongodb_ami_id
+  mongodb_bastion_subnet_id           =   var.mongodb_bastion_subnet_id
+  mongodb_bastion_instance_type       =   var.mongodb_bastion_instance_type
+  mongodb_bastion_keypair_name        =   var.mongodb_bastion_keypair_name
+
+  // MongoDB Config Server
+  mongodb_keypair_name                =   var.mongodb_keypair_name
+  mongodb_config_instance_type        =   var.mongodb_config_instance_type
+  mongodb_config_server               =   var.mongodb_config_server
+
+  // MongoDB Replica Set Members
+  mongodb_replica_set_members         =   var.mongodb_replica_set_members
+
+  // Security Group
+  mongodb_bastion_security_group_ids  =   [module.mongodb_security_group.mongodb_bastion_sg_id]
+  mongodb_security_group_ids          =   [module.mongodb_security_group.mongodb_internal_sg_id, module.mongodb_security_group.mongodb_app_sg_id]
+
+  depends_on                          =   [module.mongodb_security_group]
+}
